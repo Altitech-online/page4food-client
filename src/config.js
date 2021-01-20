6d5c9b043a3711da5dde7cd3af91e97b5fb8fc32
Stephen Palmer <stephen.palmer@coveainsurance.co.uk>
2021-01-20 15:33:14 +0000

    added signup and sign in

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ const config = {
   cognito: {
     REGION: "eu-west-2",
     USER_POOL_ID: "eu-west-2_485qBVdko",
-    APP_CLIENT_ID: "27hqcu52kr7reu56a6an0qlsc7",
-    IDENTITY_POOL_ID: "eu-west-2:74d6d6d0-e66d-4795-9654-c7c0f7e24211",
+    APP_CLIENT_ID: "pvib4g9beh08utp34ba4cr33k",
+    IDENTITY_POOL_ID: "eu-west-2:e1aac099-2844-4fea-a4e2-3311ac51593d",
   },
 };
 
